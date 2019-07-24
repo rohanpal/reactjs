9e68a4d26bc2f64269b2cfc42f883e78a7879a5f
unknown <rohansp00@gmail.com>
2019-07-24 11:24:08 +0530

    Layout with styles

---
+++ src/pages/homepage/homepage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./homepage.scss";
+import Directory from "../../components/directory/directory";
+function homepage() {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+}
+
+export default homepage;
